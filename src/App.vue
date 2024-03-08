c15a2c1a5543eac27aa8c5bc23e0e1504faea149
Shane Poppleton <shane@bjja.com.au>
2024-03-08 13:19:17 +1000

    v1.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import { getVersion } from '@tauri-apps/api/app';
+import {onMounted, ref} from 'vue';
 
-const appVersion = await getVersion();
+const appVersion = ref('');
+
+const fetchAppVersion = async() => {
+    appVersion.value = await getVersion();
+}
+
+onMounted(fetchAppVersion);
 
 import Greet from "./components/Greet.vue";
 </script>
 
 <template>
   <div class="container">
-    <h1>Welcome to Todo App! {{appVersion }}</h1>
-
+    <h1>Welcome to Todo App! {{ appVersion }}</h1>
+    Testing
     <Greet />
   </div>
 </template>
