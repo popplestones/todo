0e439ee27ffec41cc9f0d161a4c744717d1d8f07
Shane Poppleton <shane@bjja.com.au>
2024-03-08 11:46:38 +1000

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Greet from "./components/Greet.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h1>Welcome to Todo App! v0.1</h1>
+
+    <Greet />
+  </div>
+</template>
+
