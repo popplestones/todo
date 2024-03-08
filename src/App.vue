b636b1bba0374a180ac50c65735b964dd939a452
Shane Poppleton <shane@bjja.com.au>
2024-03-08 13:01:31 +1000

    v0.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+import { getVersion } from '@tauri-apps/api/app';
+
+const appVersion = await getVersion();
+
 import Greet from "./components/Greet.vue";
 </script>
 
 <template>
   <div class="container">
-    <h1>Welcome to Todo App! v0.1</h1>
+    <h1>Welcome to Todo App! {{appVersion }}</h1>
 
     <Greet />
   </div>
